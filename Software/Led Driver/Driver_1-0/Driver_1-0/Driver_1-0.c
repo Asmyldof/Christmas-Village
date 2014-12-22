cbeab6c748db375a36772b51ece9c3c062426370
Robert van Leeuwen <Post@asmyldof.com>
2014-12-22 21:08:38 +0100

    Comments with pin configuration and temporary Set-All-LEDs-On code for DEMO

--- Software/Led Driver/Driver_1-0/Driver_1-0/Driver_1-0.c
+++ Software/Led Driver/Driver_1-0/Driver_1-0/Driver_1-0.c
@@ -4,14 +4,29 @@
  * Created: 22-12-2014 20:54:38
  *  Author: Robert
  */ 
-
+// PORTB = output 1 through 8
+// PORTD6 = output 9
+// PORTD5 = output 10
+// PORTD4 = Random Bitstream Positive
+// PORTD3 / INT1 = Random Bitstream Negative
+// PORTD2 / INT0 = Night Mode input
+// PORTD0 / RXD = Serial interface
+// PORTD1 / TXD = Serial interface
+// PORTA2 / RESET = Random / Pattern select
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 int main(void)
 {
-    while(1)
+    
+	DDRB = 0xFF;
+	DDRD = 0x60;
+	
+	PORTB = 0xFF;
+	PORTD = 0x60;
+	
+	while(1)
     {
         //TODO:: Please write your application code 
     }
